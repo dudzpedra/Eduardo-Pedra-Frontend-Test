337990b186390484dd57cb0baf1960b425a65a61
Dudzpedra <poliveiraeduardo@gmail.com>
2022-05-17 13:19:08 -0300

    refactored AttributeSet styles

--- src/components/AttributeSet/index.jsx
+++ src/components/AttributeSet/index.jsx
@@ -3,14 +3,42 @@ import { AttributeContent } from "./styles/Content";
 import { AttributeWrapper } from "./styles/Wrapper";
 
 class AttributeSet extends Component {
+  /* constructor(props) {
+    super(props);
+    this.state = {
+      isSelected: false,
+    };
+  }
+  handleClick = ({ target }) => {
+    if (this.state.isSelected) {
+      this.setState({
+        isSelected: false,
+      });
+      target.style.background = "#fff";
+      target.style.color = "#000";
+    } else {
+      this.setState({
+        isSelected: true
+      })
+      target.style.background = "#000";
+      target.style.color = "#fff";
+    }
+  }; */
   render() {
     return (
       <div>
         <strong>{this.props.attribute.name.toUpperCase()}:</strong>
         <AttributeWrapper>
           {this.props.attribute.items.map((item) => (
-            <AttributeContent key={item.id}>
-              {item.displayValue}
+            <AttributeContent
+              key={item.id}
+              onClick={this.handleClick}
+              /* style={{
+                background: this.state.isSelected && "#000",
+                color: this.state.isSelected && "#fff",
+              }} */
+            >
+              {item.value}
             </AttributeContent>
           ))}
         </AttributeWrapper>
--- src/components/AttributeSet/styles/Content.js
+++ src/components/AttributeSet/styles/Content.js
@@ -1,13 +1,24 @@
 import styled from "styled-components";
 
-export const AttributeContent = styled.p`
+export const AttributeContent = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 50px;
+  width: 36px;
   border: 1px solid;
-  margin-right: 1rem;
-  padding: 1rem;
+  margin-right: 0.5rem;
+  padding: 0.5rem;
+
+  &:hover {
+    background: #ccc;
+    color: #fff;
+    cursor: pointer;
+  }
+  /* 
+  &:active {
+    background: #000;
+    color: #fff;
+  } */
 `
 
 AttributeContent.displayName = 'AttributeContent'
