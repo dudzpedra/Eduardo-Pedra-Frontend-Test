405e9e5e420c61805860522a8b73d333ab00c55f
Dudzpedra <poliveiraeduardo@gmail.com>
2022-05-24 21:10:02 -0300

    feature: custom scroll bar at mini cart

--- src/components/cart/MiniCart/styles/Wrapper.js
+++ src/components/cart/MiniCart/styles/Wrapper.js
@@ -8,6 +8,18 @@ export const MiniCartWrapper = styled.div`
   gap: 1rem;
   max-height: 67vh;
   overflow: auto;
-`
+  ::-webkit-scrollbar {
+    width: 0.5rem;
+  }
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #888;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+`;
 
-MiniCartWrapper.displayName = 'MiniCartWrapper'
+MiniCartWrapper.displayName = "MiniCartWrapper";
